42938a14f46b04a9e051d8c9600da23be9390484
prashantburande <101568499+prashantburande@users.noreply.github.com>
2022-03-17 16:28:52 +0530

    Add files via upload

---
+++ swap.java
@@ -0,0 +1,16 @@
+//que 1   Wap to swap two numbers
+package ifelse;
+
+public class swap {
+
+	public static void main(String[] args) {
+		int a=10;
+        int b=20;
+        System.out.println("before swap" + a+ " "+ " "+b);    
+        int temp =a;
+        a=b;
+        b=temp;
+        System.out.println("before swap" + a+ " "+ " "+b);
+	}
+
+}
